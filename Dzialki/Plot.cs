6ea2d8e26cc41d320f85d6d12b6bebedebb8e637
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-04-15 12:37:21 +0200

    Dzialki part1

---
+++ Dzialki/Plot.cs
@@ -0,0 +1,55 @@
+﻿namespace Dzialki
+{
+    internal class Plot
+    {
+        private List<string> plot = new List<string>();
+
+        public Plot(List<string> plot)
+        {
+            this.plot = new List<string>(plot);
+        }
+
+        public void Display()
+        {
+            foreach (string line in plot)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+        }
+
+        public bool isCoveredInGrass()
+        {
+            int grassCount = 0;
+            foreach (string str in plot)
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == '*')
+                    {
+                        grassCount += 1;
+                    }
+                }
+            }
+
+            double entireField = 30 * 30f;
+
+            if (grassCount / entireField > 0.7)
+                return true;
+            else
+                return false;
+        }
+
+        public bool isMirrored()
+        {
+            // TODO
+            return true;
+        }
+
+        public int biggestRectangleSize()
+        {
+            // TODO
+            return 0;
+        }
+    }
+}
---
+++ Dzialki/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Dzialki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("dzialki.txt");
+
+
+            List<string> inputList = new List<string>();
+            List<Plot> plotList = new List<Plot>();
+
+            foreach (string line in lines)
+            {
+                if (line != "")
+                {
+                    inputList.Add(line);
+                }
+                else
+                {
+                    plotList.Add(new Plot(inputList));
+                    inputList.Clear();
+                }
+            }
+
+            // 4.1
+            // . - empty space
+            // * - grass
+            // X - terrain
+            int grassCoveredFields = 0;
+            foreach (Plot entry in plotList)
+            {
+                if (entry.isCoveredInGrass())
+                    grassCoveredFields += 1;
+            }
+            Console.WriteLine(grassCoveredFields);
+        }
+    }
+}
